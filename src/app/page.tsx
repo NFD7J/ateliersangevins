f3a7f8477b4405de671548ae2078860572961e3c
Noé <noe.fresneau@gmail.com>
2026-07-13 20:48:20 +0200

    hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default async function HomePage() {
       <section className="relative overflow-hidden">
         <div className="absolute inset-0 -z-10">
           <Image
-            src="/images/heros.jpg"
+            src="/images/hero3.jpg"
             alt=""
             fill
             priority
